4c43f7abaaa57b76058bd25e0cb80f71b4864ab9
curiosityflow <bmt49@zips.uakron.edu>
2017-10-01 23:51:36 -0400

    Team Project as Practice 5

---
+++ Team Project/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <vector>
+#include "Card.h"
+#include "Deck.h"
+// #include "Game.h"
+#include "Blackjack.h"
+
+
+int main()
+{
+	//Game g;
+	//g.run();
+
+	Blackjack b;
+	b.run_game();
+}
