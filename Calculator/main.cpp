efdb79cfbac024286a1ae807be1c6bc3a427c783
curiosityflow <bmt49@zips.uakron.edu>
2017-11-26 12:41:09 -0500

    Fixed bugs

--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -18,16 +18,16 @@ int main() {
   std::cout << " = " << my_exp3.eval() << std::endl;
 
 
-  visitor* my_v = new multiply(new add( new Int(10)), new Int(8)), new subtract(new Int(3), new Int(1)));
-  my_v.print();
-  std::cout << " = " << my_v.eval() << std::endl;
+  Expression* my_v = new multiply(new add( new Int(10)), new Int(8)), new subtract(new Int(3), new Int(1)));
+  print(my_v);
+  std::cout << " = " << eval(my_v) << std::endl;
 
-  visitor* my_v2 = new add(new multiply( new Int(6)), new Int(12)), new divide(new Int(9), new Int(3)));
-  my_v2.print();
-  std::cout << " = " << my_v2.eval() << std::endl;
+  Expression* my_v2 = new add(new multiply( new Int(6)), new Int(12)), new divide(new Int(9), new Int(3)));
+  print(my_v2);
+  std::cout << " = " << eval(my_v2) << std::endl;
 
-  visitor* my_v3 = new add(new subtract( new Int(30)), new Int(18)), new multiply(new Int(2), new Int(2)));
-  my_v3.print();
-  std::cout << " = " << my_v3.eval() << std::endl;
+  Expression* my_v3 = new add(new subtract( new Int(30)), new Int(18)), new multiply(new Int(2), new Int(2)));
+  print(my_v3);
+  std::cout << " = " << eval(my_v3) << std::endl;
 
 }
